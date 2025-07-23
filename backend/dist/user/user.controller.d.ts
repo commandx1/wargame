5bbcd9811eb7774d0185e221d946d1a33593fc8c
serhatbelen <serhat@onlay.ai>
2025-07-23 18:12:45 +0300

    Remove .env and dist files, update gitignore

--- backend/dist/user/user.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    reload(userId: string): Promise<{
-        energy_reload_time: number;
-        energy: number;
-    }>;
-}
